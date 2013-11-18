fcf9800f542c04669ccad8d29485173949418ff2
William Dauchy <william@gandi.net>
2013-11-18 16:36:23 +0100

    net: rbridge: fix header coding style
    Signed-off-by: William Dauchy <william@gandi.net>
    Signed-off-by: Ahmed Amamou <ahmed@gandi.net>

--- net/bridge/rbridge/rbr_private.h
+++ net/bridge/rbridge/rbr_private.h
@@ -22,64 +22,68 @@
 			(n) != RBRIDGE_NICKNAME_UNUSED)
 
 struct rbr_nickinfo {
-  /* Nickname of the RBridge */
-  uint16_t	nick;
-  /* Next-hop SNPA address to reach this RBridge */
-  u8	adjsnpa[ETH_ALEN];
-  /* Link on our system to use to reach next-hop */
-  uint32_t 	linkid;
-  /* Num of *our* adjacencies on a tree rooted at this RBridge */
-  uint16_t	adjcount;
-  /* Num of distribution tree root nicks chosen by this RBridge */
-  uint16_t	dtrootcount;
-  /*
-   * Variable size bytes to store adjacency nicks, distribution
-   * tree roots. Adjacency nicks and
-   * distribution tree roots are 16-bit fields.
-   */
+	/* Nickname of the RBridge */
+	uint16_t	nick;
+	/* Next-hop SNPA address to reach this RBridge */
+	u8	adjsnpa[ETH_ALEN];
+	/* Link on our system to use to reach next-hop */
+	uint32_t	linkid;
+	/* Num of *our* adjacencies on a tree rooted at this RBridge */
+	uint16_t	adjcount;
+	/* Num of distribution tree root nicks chosen by this RBridge */
+	uint16_t	dtrootcount;
+	/*
+	 * Variable size bytes to store adjacency nicks, distribution
+	 * tree roots. Adjacency nicks and
+	 * distribution tree roots are 16-bit fields.
+	 */
 };
+
 struct rbr_node {
-  struct rbr_nickinfo	*rbr_ni;
-  atomic_t		refs; /* reference count */
+	struct rbr_nickinfo	*rbr_ni;
+	atomic_t		refs; /* reference count */
 };
 
 struct rbr {
-  spinlock_t		lock;
-  char 		name[IFNAMSIZ];
-  uint16_t		nick; /* our nickname */
-  uint16_t		treeroot; /* tree root nickname */
-  struct rbr_node	*rbr_nodes[RBRIDGE_NICKNAME_MAX];
-  uint			rbr_nodecount;
-  char			rbr_bridgename[IFNAMSIZ];
-  struct net_bridge	*br;
+	spinlock_t		lock;
+	char			name[IFNAMSIZ];
+	uint16_t		nick; /* our nickname */
+	uint16_t		treeroot; /* tree root nickname */
+	struct rbr_node		*rbr_nodes[RBRIDGE_NICKNAME_MAX];
+	uint			rbr_nodecount;
+	char			rbr_bridgename[IFNAMSIZ];
+	struct net_bridge	*br;
 };
 
-static inline void rbr_node_free(struct rbr_node *rbr_node){
-  if (rbr_node!=NULL){
-    if (rbr_node->rbr_ni!=NULL)
-	kfree(rbr_node->rbr_ni);
-    kfree(rbr_node);
-  }
+static inline void rbr_node_free(struct rbr_node *rbr_node)
+{
+	if (rbr_node != NULL) {
+		if (rbr_node->rbr_ni != NULL)
+		kfree(rbr_node->rbr_ni);
+		kfree(rbr_node);
+	}
 }
+
 static inline void rbr_node_get(struct rbr_node *rbr_node)
 {
-  if (rbr_node!=NULL){
-    atomic_inc(&rbr_node->refs);
-  }
+	if (rbr_node!=NULL) {
+		atomic_inc(&rbr_node->refs);
+	}
 }
+
 static inline void rbr_node_put (struct rbr_node *rbr_node)
 {
-  if (rbr_node){
-    if (likely(atomic_dec_and_test(&rbr_node->refs))){
-	rbr_node_free(rbr_node);
-    }
-  }
+	if (rbr_node) {
+		if (likely(atomic_dec_and_test(&rbr_node->refs)))
+			rbr_node_free(rbr_node);
+	}
 }
 
 extern int set_treeroot(struct rbr *rbr, uint16_t treeroot);
 extern struct rbr_node *rbr_find_node(struct rbr* rbr, __u16 nickname);
 extern int rbr_del_node(struct rbr *rbr, uint16_t nickname);
 extern void rbr_del_all(struct rbr *rbr);
+
 /* Access the adjacency nick list at the end of rbr_nickinfo */
 #define	RBR_NI_ADJNICKSPTR(v) ((uint16_t *)((struct rbr_nickinfo *)(v)+1))
 #define	RBR_NI_ADJNICK(v, n) (RBR_NI_ADJNICKSPTR(v)[(n)])
