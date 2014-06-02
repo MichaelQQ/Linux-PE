1586f438207b62d1feea56e5bc71493ba16c4543
Ahmed Amamou <ahmed@gandi.net>
2014-06-02 10:27:04 +0200

    net: igb: missing ntohs convertion when getting th_flags
    Signed-off-by: Jean-Loup Colautti <jloup@gandi.net>
    Signed-off-by: William Dauchy <william@gandi.net>
    Signed-off-by: Ahmed Amamou <ahmed@gandi.net>

--- drivers/net/ethernet/intel/igb/igb_main.c
+++ drivers/net/ethernet/intel/igb/igb_main.c
@@ -6502,7 +6502,7 @@ static unsigned int igb_get_headlen(unsigned char *data,
 	if (protocol == __constant_htons(ETH_P_TRILL)) {
 		if ((hdr.network - data) > (max_len - sizeof(struct trill_hdr)))
 			return max_len;
-		trill_op_len = trill_get_optslen(hdr.trill->th_flags);
+		trill_op_len = trill_get_optslen(ntohs(hdr.trill->th_flags));
 		hdr.network += sizeof(struct trill_hdr);
 		if (trill_op_len) {
 			if ((hdr.network - data) > (max_len - trill_op_len))
