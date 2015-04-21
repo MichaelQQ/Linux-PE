0b92ae67836e33dd8c842263e59b002484bf5b67
MichaelQQ <understatedmic20@gmail.com>
2015-04-21 15:30:10 +0800

    add mpls_ptr in include/linux/netdevice.h

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1292,6 +1292,7 @@ struct net_device {
 	void			*ax25_ptr;	/* AX.25 specific data */
 	struct wireless_dev	*ieee80211_ptr;	/* IEEE 802.11 specific data,
 						   assign before registering */
+	void			*mpls_ptr;	/* MPLS specific data */
 
 /*
  * Cache lines mostly used on receive path (including eth_type_trans())
